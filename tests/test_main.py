bd950733781cc5941e1803b954e90acf36ac74c7
Renjith Baby <139134056+renjith-digicat@users.noreply.github.com>
2024-10-24 10:39:22 +0100

    add unit test for model endpoint payload (#10)
    # Pull Request
    
    ## Checklist
    - [x] Have you read Digital Catapult's [Code of
    Conduct](https://github.com/digicatapult/.github/blob/main/CODE_OF_CONDUCT.md)?
    - [x] I have performed a self-review of my own code.
    - [x] I have commented my code, particularly in hard-to-understand
    areas.
    - [x] I have made corresponding changes to the documentation.
    - [x] My changes generate no new warnings.
    - [ ] I have added tests that prove my fix is effective or that my
    feature works.
    - [ ] New and existing unit tests pass locally with my changes.
    
    ## PR Type
    
    - [x] Chore
    - [x] Documentation Update
    
    
    ## Linked tickets
    
    https://digicatapult.atlassian.net/browse/BRID-118
    
    ## High level description
    
    Add unit tests for checking the input payload to the kserve deployed
    model endpount.
    
    ## Detailed description
    
    <!-- A detailed description of the feature and if possible describe how
    has been implemented. -->
    
    
    ## Describe alternatives you've considered
    
    <!-- A clear and concise description of the alternative solutions you've
    considered. Be sure to explain why the existing customisability isn't
    suitable for this feature. -->
    
    ## Operational impact
    
    <!--- A description of any operational considerations associated with
    the change. Is there anything in particular we should be looking at when
    deploying the change to make sure it is working as intended. If
    something goes wrong will any special actions be needed to revert the
    change. -->
    
    ## Additional context
    
    <!-- Add any other context or screenshots about the feature request
    here. -->

--- tests/test_main.py
+++ tests/test_main.py
@@ -30,6 +30,84 @@ PAYLOAD = {
     "parking": 1,
 }
 
+# Payload in the format that the model expects
+MODEL_EXPECTED_PAYLOAD = {
+    "inputs": [
+        {
+            "name": "mainroad",
+            "shape": [1],
+            "datatype": "BYTES",
+            "data": [PAYLOAD["mainroad"].upper()],
+        },
+        {
+            "name": "guestroom",
+            "shape": [1],
+            "datatype": "BYTES",
+            "data": [PAYLOAD["guestroom"].upper()],
+        },
+        {
+            "name": "basement",
+            "shape": [1],
+            "datatype": "BYTES",
+            "data": [PAYLOAD["basement"].upper()],
+        },
+        {
+            "name": "hotwaterheating",
+            "shape": [1],
+            "datatype": "BYTES",
+            "data": [PAYLOAD["hotwaterheating"].upper()],
+        },
+        {
+            "name": "airconditioning",
+            "shape": [1],
+            "datatype": "BYTES",
+            "data": [PAYLOAD["airconditioning"].upper()],
+        },
+        {
+            "name": "prefarea",
+            "shape": [1],
+            "datatype": "BYTES",
+            "data": [PAYLOAD["prefarea"].upper()],
+        },
+        {
+            "name": "furnishingstatus",
+            "shape": [1],
+            "datatype": "BYTES",
+            "data": [PAYLOAD["furnishingstatus"].upper()],
+        },
+        {
+            "name": "area",
+            "shape": [1],
+            "datatype": "FP32",
+            "data": [PAYLOAD["area"]],
+        },
+        {
+            "name": "bedrooms",
+            "shape": [1],
+            "datatype": "INT64",
+            "data": [PAYLOAD["bedrooms"]],
+        },
+        {
+            "name": "bathrooms",
+            "shape": [1],
+            "datatype": "INT64",
+            "data": [PAYLOAD["bathrooms"]],
+        },
+        {
+            "name": "stories",
+            "shape": [1],
+            "datatype": "INT64",
+            "data": [PAYLOAD["stories"]],
+        },
+        {
+            "name": "parking",
+            "shape": [1],
+            "datatype": "INT64",
+            "data": [PAYLOAD["parking"]],
+        },
+    ]
+}
+
 engine = create_engine(DB_URL)
 TestingSessionLocal = sessionmaker(
     autocommit=False, autoflush=False, bind=engine
@@ -120,3 +198,51 @@ def test_predict_logs_to_db(mock_post):
     assert log.mainroad == PAYLOAD["mainroad"]
     assert log.bedrooms == PAYLOAD["bedrooms"]
     assert log.prediction_response == price
+
+
+@patch("src.main.requests.post")
+def test_get_prediction_post_to_model_endpoint(mock_post):
+    """Test to check if the model endpoint gets the correct payload."""
+    # Mock the response
+    mock_post.return_value.status_code = 200
+    price = 500000.0
+    mock_post.return_value.json.return_value = {
+        "model_name": "house_price_prediction_prod",
+        "model_version": "4",
+        "id": "fa48be92-fa79-4b25-b305-b46c0f877893",
+        "parameters": {"content_type": "np"},
+        "outputs": [
+            {
+                "name": "output-1",
+                "shape": [1, 1],
+                "datatype": "FP32",
+                "parameters": {"content_type": "np"},
+                "data": [price],
+            }
+        ],
+    }
+    # Send a POST request to the /predict endpoint
+    response = client.post("/predict", json=PAYLOAD)
+
+    # Assert the response from /predict is successful
+    assert response.status_code == 200
+
+    # Assert that the model endpoint was called with the correct payload
+    mock_post.assert_called_once()
+
+    # Extract the actual payload sent to the mocked model endpoint
+    actual_payload = mock_post.call_args[1]["json"]
+
+    # Function to sort the inputs list by the 'name' key
+    def sort_inputs(payload):
+        payload["inputs"] = sorted(payload["inputs"], key=lambda x: x["name"])
+        return payload
+
+    expected_sorted_inputs = sort_inputs(MODEL_EXPECTED_PAYLOAD)
+    actual_sorted_inputs = sort_inputs(actual_payload)
+
+    # Assert that the actual sorted payload matches the expected sorted payload
+    assert actual_sorted_inputs == expected_sorted_inputs, (
+        f"Expected payload: \n{expected_sorted_inputs},"
+        f"\nbut got: \n{actual_sorted_inputs}"
+    )
